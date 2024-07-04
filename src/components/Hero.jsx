bba08a4bf5d9043b90d36c0eaac69cc9f5678881
Goody-23 <goodnessibikunle@gmail.com>
2024-07-04 17:33:40 -0400

    React portfolio commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,59 @@
+import { HERO_CONTENT } from "../constants";
+import profilePic from "../assets/kevinRushProfile.png";
+import { motion } from "framer-motion"; 
+
+const container = (delay) => ({
+  hidden: {x: -100, opacity: 0 },
+  visible: {
+    x:0,
+    opacity: 1,
+    transition: {duration: 0.5, delay: delay},
+  },
+})
+
+const Hero = () => {
+  return (
+    <div className="border-b border-neutral-900 p-4 lg:mb-35">
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start">
+            <motion.h1 
+            variants={container(0)}
+            initial="hidden"
+            animate="visible"
+            className="pb-16 text-6xl font-thin tracking-tight lg:mb-16 
+            lg:text-8xl"
+            >
+              Goodness Ibikunle
+            </motion.h1>
+            <motion.span 
+            variants={container(0.5)}
+            initial="hidden"
+            animate="visible"
+            className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 
+             bg-clip-text text-3xl tracking-tight text-transparent"
+             >
+              Software Engineer
+            </motion.span>
+            <motion.p 
+            variants={container(1)}
+            initial="hidden"
+            animate="visible" className="my-2 max-w-xl py-6 font-light 
+            tracking-tighter">
+              {HERO_CONTENT}
+            </motion.p>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <motion.img 
+          initial= {{ x: 100, opacity: 0}}
+          animate= {{x: 0, opacity: 1 }}
+          transition={{duration: 1, delay: 1.2 }}
+          src={profilePic} alt="Kevin Rush" className="max-w-xs lg:max-w-sm mx-auto" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/constants/index.js
@@ -0,0 +1,63 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = 
+`I am a passionate SWE with a knack for crafting robust and scalable web applications. As of right now with the experience ive gotten, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile SWE with a passion for creating efficient and user-friendly web applications. As of right now with the experience ive gotten, I have worked with a variety of technologies, including React, Next.js and Node.js. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, physically mentally etc, also exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Software Engineering Apprentice",
+    company: "Codepath.",
+    description: `Engaged in collaborative coding projects, applying data structures and algorithms to real-world scenarios and practical problems, enhancing ability to work effectively in a group setting`,
+    technologies: ["Javascript", "React.js", "Next.js"],
+  },
+  {
+    year: "Jan 2024 - April 2024",
+    role: "Frontend Engineer",
+    company: "Bool LLC",
+    description: `Designed and developed user friendly Website for college students to predict bar/club wait times using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Node.js", "Next.js", "React.js"],
+  },
+  
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  phoneNo: "267-298-0406 ",
+  email: "goodnessibikunle@gmail.com",
+};
