feea189466e72e2e01801839d4c4ba90d9783e74
Goody-23 <152367610+Goody-23@users.noreply.github.com>
2024-07-16 00:26:21 -0400

    Projects update
    Started working on some projects so i'm adding it to my website

--- src/constants/index.js
+++ src/constants/index.js
@@ -28,19 +28,13 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Task Management App",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "A fully functional Task manager application/Website featuring user authentication. This application allows users to efficiently manage their tasks with secure login, responsive design, and a user-friendly interface.",
+    technologies: ["Angular,", "Node.js", "MongoDB"],
   },
-  {
-    title: "Task Management App",
-    image: project2,
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
-  },
+  
   {
     title: "Portfolio Website",
     image: project3,
@@ -49,6 +43,12 @@ export const PROJECTS = [
     technologies: ["HTML", "CSS", "React", "Bootstrap"],
   },
   {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
     title: "Blogging Platform",
     image: project4,
     description:
