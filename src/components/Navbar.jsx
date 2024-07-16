dde4ecb7f80ced693daca477f47da13c4cbc232d
Goody-23 <goodnessibikunle@gmail.com>
2024-07-16 00:19:45 -0400

    Git/linkedln link, update on projects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,23 @@
 import logo from "../assets/kevinRushLogo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+
 const Navbar = () => {
-  return <nav className="mb-20 flex items-center justify-between py-6">
-    <div className="flex flex-shrink-0 items-center">
-        <img className="ml-1 mr-2 w-12"src={logo} alt="logo" />
-    </div>
-    <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-       <FaLinkedin />
-       <FaGithub />
-    </div>
-  </nav>
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="ml-1 mr-2 w-12" src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://www.linkedin.com/in/goodness-ibikunle/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/Goody-23" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+      </div>
+    </nav>
+  );
 };
 
-export default Navbar
+export default Navbar;
