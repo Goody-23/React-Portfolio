53a18985c8a3a64180e27d65f273bc9caa5979f4
Goody-23 <152367610+Goody-23@users.noreply.github.com>
2024-07-16 00:34:54 -0400

    Logo reload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,9 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img className="ml-1 mr-2 w-12" src={logo} alt="logo" />
+        <a href="/">
+          <img className="ml-1 mr-2 w-12" src={logo} alt="logo" />
+        </a>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <a href="https://www.linkedin.com/in/goodness-ibikunle/" target="_blank" rel="noopener noreferrer">
